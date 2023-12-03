d60d2ed86a5cbbafb74a1bb490b5806f9b51196c
online5880 <gjtjqkr1@naver.com>
2023-12-04 00:57:53 +0900

    init

---
+++ Source/Project_D/Project_DGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_D_API AProject_DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
