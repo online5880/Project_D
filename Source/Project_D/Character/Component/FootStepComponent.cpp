b64b6eaeebd260902e5839ab31b57cd12e86ac58
online5880 <gjtjqkr1@naver.com>
2023-12-25 22:55:49 +0900

    FootStepComponent 생성
    - Sound, Effect 등등 담당할 액터 컴포넌트

---
+++ Source/Project_D/Character/Component/FootStepComponent.cpp
@@ -0,0 +1,32 @@
+﻿#include "FootStepComponent.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FootStepComponent)
+
+UFootStepComponent::UFootStepComponent()
+{
+
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UFootStepComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UFootStepComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+void UFootStepComponent::FootStep()
+{
+}
+
+void UFootStepComponent::FootStepSphereTrace()
+{
+}
+
---
+++ Source/Project_D/Character/Component/FootStepComponent.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Components/ActorComponent.h"
+#include "FootStepComponent.generated.h"
+
+
+/**
+ * Foot Step Component
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECT_D_API UFootStepComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFootStepComponent();
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Foot Step(Sound, Effect...)
+	void FootStep();
+protected:
+
+	// Sphere Trace
+	void FootStepSphereTrace();
+private:
+	UPROPERTY()
+	TObjectPtr<ACharacter> Character;
+	
+};
