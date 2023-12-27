18e77888f600395a3d6554e4a95cb4acb7a66623
online5880 <gjtjqkr1@naver.com>
2023-12-27 21:13:52 +0900

    애니메이션 노티파이 추가
    - Foot Right
    - Foot Left

---
+++ Source/Project_D/Object/Weapon/Ranged/BaseRangedWeapon.cpp
@@ -0,0 +1,42 @@
+﻿#include "BaseRifle.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(BaseRifle)
+
+ABaseRifle::ABaseRifle()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABaseRifle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABaseRifle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABaseRifle::Attack()
+{
+	Super::Attack();
+}
+
+void ABaseRifle::Reload()
+{
+	Super::Reload();
+}
+
+void ABaseRifle::OnBeginOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                     UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnBeginOverlapEvent(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void ABaseRifle::OnEndOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnEndOverlapEvent(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
+
---
+++ Source/Project_D/Object/Weapon/Ranged/BaseRangedWeapon.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Project_D/Object/Weapon/BaseWeapon.h"
+#include "BaseRifle.generated.h"
+
+UCLASS()
+class PROJECT_D_API ABaseRifle : public ABaseWeapon
+{
+	GENERATED_BODY()
+
+public:
+
+	ABaseRifle();
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	// IAttackComponent
+	virtual void Attack() override;
+	virtual void Reload() override;
+
+	// Overlap
+	virtual void OnBeginOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnEndOverlapEvent(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+public:
+
+	
+};
